51f4d45ec7bc7996aa0bd21fd712c38301365ee7
gamosoft <gamosoft@users.noreply.github.com>
2020-08-09 19:46:05 +0200

    Layout WIP

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 const albumUrl = 'http://my-json-server.typicode.com/gamosoft/GMusicSPA/albums';
 
+function trim(value, length) {
+    return value.length > length
+            ? value.substring(0, length - 3) + "..."
+            : value;
+}
+
 async function loadAlbums() {
     // Fetch album information
     const response = await fetch(`${albumUrl}`);
