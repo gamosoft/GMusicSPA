3ddb0cd8777768cd8451669e8f6735e5bc492542
gamosoft <gamosoft@users.noreply.github.com>
2020-08-15 20:14:36 +0200

    WIP styling mini-player

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 $(() => {
     ko.applyBindings(ViewModel, document.getElementById('body'));
 
-    ViewModel.CurrentSong({ title: "-", artist: "-", album: "-" }); // TODO: Hack to make the progressbar fix. Will go away after proper markup
+    ViewModel.CurrentSong({
+        title: "Some song",
+        artist: "Steve Vai",
+        album: "This is the album",
+        image: "https://upload.wikimedia.org/wikipedia/en/6/6d/SteveVaiAlienLoveSecrets.jpg"
+    }); // TODO: Hack to make the progressbar fix. Will go away after proper markup
 
     app.run('#/albums');
 
