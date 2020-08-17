bbb85ba66bc8e08b4c877131fef118a9510f8bc7
gamosoft <gamosoft@users.noreply.github.com>
2020-08-17 08:17:30 +0200

    todo

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -271,6 +271,8 @@ MediaPlayerViewModel = (function () {
 
 TODO: video thumbnail, iOS lock screen
 
+Several viewmodels same element?
+
 .volume = 0.0 - 1.0
 .ontimeupdate = function
 .ended (property and event)
